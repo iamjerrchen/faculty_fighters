542a1c074d3040a3dc8e8d399a9f87a15d6d9e0b
jerr-chen <jerrychen31@gmail.com>
2018-04-15 01:47:46 -0500

    Cleaned up top level and isolated USB code. keystrokes are ooutputed to FPGA properly.

--- faculty_fighter_top_level.sv
+++ faculty_fighter_top_level.sv
@@ -1,50 +1,39 @@
-//-------------------------------------------------------------------------
-//      lab8.sv                                                          --
-//      Christine Chen                                                   --
-//      Fall 2014                                                        --
-//                                                                       --
-//      Modified by Po-Han Huang                                         --
-//      10/06/2017                                                       --
-//                                                                       --
-//      Fall 2017 Distribution                                           --
-//                                                                       --
-//      For use with ECE 385 Lab 8                                       --
-//      UIUC ECE Department                                              --
-//-------------------------------------------------------------------------
-
-
-module faculty_fighter_top_level( input               CLOCK_50,
-             input        [3:0]  KEY,          //bit 0 is set up as Reset
-             output logic [6:0]  HEX0, HEX1,
-             // VGA Interface 
-             output logic [7:0]  VGA_R,        //VGA Red
-                                 VGA_G,        //VGA Green
-                                 VGA_B,        //VGA Blue
-             output logic        VGA_CLK,      //VGA Clock
-                                 VGA_SYNC_N,   //VGA Sync signal
-                                 VGA_BLANK_N,  //VGA Blank signal
-                                 VGA_VS,       //VGA virtical sync signal
-                                 VGA_HS,       //VGA horizontal sync signal
-             // CY7C67200 Interface
-             inout  wire  [15:0] OTG_DATA,     //CY7C67200 Data bus 16 Bits
-             output logic [1:0]  OTG_ADDR,     //CY7C67200 Address 2 Bits
-             output logic        OTG_CS_N,     //CY7C67200 Chip Select
-                                 OTG_RD_N,     //CY7C67200 Write
-                                 OTG_WR_N,     //CY7C67200 Read
-                                 OTG_RST_N,    //CY7C67200 Reset
-             input               OTG_INT,      //CY7C67200 Interrupt
-             // SDRAM Interface for Nios II Software
-             output logic [12:0] DRAM_ADDR,    //SDRAM Address 13 Bits
-             inout  wire  [31:0] DRAM_DQ,      //SDRAM Data 32 Bits
-             output logic [1:0]  DRAM_BA,      //SDRAM Bank Address 2 Bits
-             output logic [3:0]  DRAM_DQM,     //SDRAM Data Mast 4 Bits
-             output logic        DRAM_RAS_N,   //SDRAM Row Address Strobe
-                                 DRAM_CAS_N,   //SDRAM Column Address Strobe
-                                 DRAM_CKE,     //SDRAM Clock Enable
-                                 DRAM_WE_N,    //SDRAM Write Enable
-                                 DRAM_CS_N,    //SDRAM Chip Select
-                                 DRAM_CLK      //SDRAM Clock
-                    );
+module faculty_fighter_top_level(
+											input	CLOCK_50,
+											input        [3:0]  	KEY,          //bit 0 is set up as Reset
+											output logic [6:0]  	HEX0, HEX1,
+											
+											// VGA Interface 
+											output logic [7:0]  	VGA_R,        //VGA Red
+																		VGA_G,        //VGA Green
+																		VGA_B,        //VGA Blue
+											output logic			VGA_CLK,      //VGA Clock
+																		VGA_SYNC_N,   //VGA Sync signal
+																		VGA_BLANK_N,  //VGA Blank signal
+																		VGA_VS,       //VGA virtical sync signal
+																		VGA_HS,       //VGA horizontal sync signal
+											
+											// CY7C67200 Interface
+											inout  wire  [15:0] 	OTG_DATA,     //CY7C67200 Data bus 16 Bits
+											output logic [1:0]  	OTG_ADDR,     //CY7C67200 Address 2 Bits
+											output logic        	OTG_CS_N,     //CY7C67200 Chip Select
+																		OTG_RD_N,     //CY7C67200 Write
+																		OTG_WR_N,     //CY7C67200 Read
+																		OTG_RST_N,    //CY7C67200 Reset
+											input               	OTG_INT,      //CY7C67200 Interrupt
+											
+											// SDRAM Interface for Nios II Software
+											output logic [12:0] 	DRAM_ADDR,    //SDRAM Address 13 Bits
+											inout  wire  [31:0] 	DRAM_DQ,      //SDRAM Data 32 Bits
+											output logic [1:0]  	DRAM_BA,      //SDRAM Bank Address 2 Bits
+											output logic [3:0]  	DRAM_DQM,     //SDRAM Data Mast 4 Bits
+											output logic        	DRAM_RAS_N,   //SDRAM Row Address Strobe
+																		DRAM_CAS_N,   //SDRAM Column Address Strobe
+																		DRAM_CKE,     //SDRAM Clock Enable
+																		DRAM_WE_N,    //SDRAM Write Enable
+																		DRAM_CS_N,    //SDRAM Chip Select
+																		DRAM_CLK      //SDRAM Clock
+											);
     
     logic Reset_h, Clk;
     logic [7:0] keycode;
@@ -79,8 +68,8 @@ module faculty_fighter_top_level( input               CLOCK_50,
                             .OTG_RST_N(OTG_RST_N)
     );
      
-     // You need to make sure that the port names here match the ports in Qsys-generated codes.
-     faculty_fighter_soc nios_system(
+    // You need to make sure that the port names here match the ports in Qsys-generated codes.
+    faculty_fighter_soc nios_system(
                              .clk_clk(Clk),         
                              .reset_reset_n(1'b1),    // Never reset NIOS
                              .sdram_wire_addr(DRAM_ADDR), 
@@ -105,12 +94,12 @@ module faculty_fighter_top_level( input               CLOCK_50,
     
     // Use PLL to generate the 25MHZ VGA_CLK.
     // You will have to generate it on your own in simulation.
-    vga_clk vga_clk_instance(.inclk0(Clk), .c0(VGA_CLK));
+    //vga_clk vga_clk_instance(.inclk0(Clk), .c0(VGA_CLK));
     
-	 logic [9:0] DrawX, DrawY;
-	 logic is_ball;
+	 //logic [9:0] DrawX, DrawY;
+	 //logic is_ball;
     // TODO: Fill in the connections for the rest of the modules 
-    VGA_controller vga_controller_instance(.Clk(Clk),
+    /*VGA_controller vga_controller_instance(.Clk(Clk),
 														.Reset(Reset_h),
 														.VGA_HS(VGA_HS),
 														.VGA_VS(VGA_VS),
@@ -118,32 +107,26 @@ module faculty_fighter_top_level( input               CLOCK_50,
 														.VGA_BLANK_N(VGA_BLANK_N),
 														.VGA_SYNC_N(VGA_SYNC_N),
 														.DrawX(DrawX),
-														.DrawY(DrawY));
+														.DrawY(DrawY));*/
     
     // Which signal should be frame_clk?
-    ball ball_instance(.Clk(Clk),
+    /*ball ball_instance(.Clk(Clk),
 								.Reset(Reset_h),
 								.frame_clk(VGA_VS),
 								.keycode(keycode),
 								.DrawX(DrawX),
 								.DrawY(DrawY),
-								.is_ball(is_ball));
+								.is_ball(is_ball));*/
     
-    color_mapper color_instance(.is_ball(is_ball),
+    /*color_mapper color_instance(.is_ball(is_ball),
 											.DrawX(DrawX),
 											.DrawY(DrawY),
 											.VGA_R(VGA_R),
 											.VGA_G(VGA_G),
-											.VGA_B(VGA_B));
+											.VGA_B(VGA_B));*/
     
     // Display keycode on hex display
     HexDriver hex_inst_0 (keycode[3:0], HEX0);
     HexDriver hex_inst_1 (keycode[7:4], HEX1);
     
-    /**************************************************************************************
-        ATTENTION! Please answer the following quesiton in your lab report! Points will be allocated for the answers!
-        Hidden Question #1/2:
-        What are the advantages and/or disadvantages of using a USB interface over PS/2 interface to
-             connect to the keyboard? List any two.  Give an answer in your Post-Lab.
-    **************************************************************************************/
 endmodule
